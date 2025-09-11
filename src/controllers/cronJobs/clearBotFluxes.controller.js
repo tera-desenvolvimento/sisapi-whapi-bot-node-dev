194aa1a543b2ce4b86bbfd2edc600f22de124f23
Suporte Tera <suporte@tera.tec.br>
2025-09-11 08:21:37 -0300

    Testando cronjob

--- src/controllers/cronJobs/clearBotFluxes.controller.js
+++ src/controllers/cronJobs/clearBotFluxes.controller.js
@@ -4,7 +4,8 @@ const clearFluxes = require('../flux/clearFluxes.controller');
 function clearBotFluxes() {
     // Limpando fluxos de whatsapp em aberto todo dia, 2h da madrugada
     const rule = new schedule.RecurrenceRule();
-    rule.hour = 2;
+    rule.hour = 8;
+    rule.minute = 30;
 
     const job = schedule.scheduleJob(rule, async function(){
         await clearFluxes();
